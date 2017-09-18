1eef2262ae13ad0abef9469ca3e3b6174696a5ed
ypoler <ypoler@yahoo.com>
2017-09-19 01:41:18 +0300

    added random basic shapes (no sound yet)

--- pygame_demo.py
+++ pygame_demo.py
@@ -3,7 +3,65 @@ from pygame.locals import *
 from random import randint
 
 
+MAX_ACTIVITY = 3	
+MIN_SHAPE_SIZE = 100
+COL_WHITE = (255, 255, 255)
+COL_BLACK = (0, 0, 0)
 
+COLOR_ARR = [ (255, 0, 0), # R
+              (0, 255, 0), # G
+			  (0, 0, 255), # B
+			  (255, 255, 0), # Yellow
+              (255, 255, 255), #white
+              (255, 165, 0)  # Orange
+ 			  ]
+
+def draw_circle(surf, x, y, r, col):
+    """
+    Draws a circle
+    """
+    pygame.draw.circle(surf, col, (x, y), r)
+    pygame.draw.circle(surf, COL_WHITE, (x, y), r+1, 1)
+
+
+def draw_rect(surf, x, y, w, h, col):
+    """
+    Draws a rectangle
+    """
+    pygame.draw.rect(surf, col, (x, y, w, h))
+    pygame.draw.rect(surf, COL_WHITE, (x-1, y-1, w+2, h+2), 1)
+
+
+def draw_triangle(surf, x, y, r, col):
+    """
+    Draws a triangle (all sides are same, all 60 degrees
+    """	
+    pygame.draw.polygon(surf, col, ((x, y-r), (x+r*0.86, y+r*0.5), (x-r*0.86, y+r*0.5)) )
+    pygame.draw.polygon(surf, COL_WHITE, ((x, y-1-r), (x+1+r*0.86, y+1+r*0.5), (x-1-r*0.86, y+1+r*0.5)), 1 )
+
+	
+
+def draw_random_activity(surf, max_x, max_y, max_size):
+    """
+	Draw a random activity (shape, ...)
+	"""
+	
+    pygame.draw.rect(surf, COL_BLACK, (0, 0, max_x, max_y))        # Delete the previous screen (draw big black rect)
+	
+    x = randint(MIN_SHAPE_SIZE*2, max_x-MIN_SHAPE_SIZE*2-1)
+    y = randint(MIN_SHAPE_SIZE*2, max_y-MIN_SHAPE_SIZE*2-1)
+    r = randint(MIN_SHAPE_SIZE, MIN_SHAPE_SIZE+max_size)
+    col = COLOR_ARR[ randint(0, len(COLOR_ARR)-1) ] # (255, 0, 0)
+    activity = randint(0, MAX_ACTIVITY-1)
+	
+    if (activity == 0):
+        draw_circle(surf, x, y, r, col)
+    elif (activity == 1):
+        draw_rect(surf, x, y, r, randint(MIN_SHAPE_SIZE, MIN_SHAPE_SIZE+r-1), col)
+    elif (activity == 2):
+        draw_triangle(surf, x, y, r, col)
+
+		
 pygame.init()
 #DISPLAYSURF = pygame.display.set_mode((400, 300), pygame.FULLSCREEN)
 info = pygame.display.Info()
@@ -14,20 +72,16 @@ max_y = info.current_h
 DISPLAYSURF = pygame.display.set_mode((max_x, max_y), pygame.FULLSCREEN)
 pygame.display.set_caption('Hello World!')
 
-RED = (255, 0, 0)
-pygame.draw.rect(DISPLAYSURF, RED, (200, 150, 100, 50))
+
 
 while True: # main game loop
     for event in pygame.event.get():
-	    if event.type == KEYDOWN:
-	        if event.key == K_SPACE:
-      #  if event.type == QUIT:
-	            pygame.quit()
-	            sys.exit()
-            else:
-		        x = randint(0, max_x-1)
-		        y = randint(0, max_y-1)
-		        pygame.draw.rect(DISPLAYSURF, RED, (x, y, x+59, y+20))
+	    if (event.type == KEYDOWN):
+		    draw_random_activity(DISPLAYSURF, max_x, max_y, 200)
 
+	    elif (event.type == QUIT):
+	        print("Quiting")
+	        pygame.quit()
+	        sys.exit()   
 
     pygame.display.update()
