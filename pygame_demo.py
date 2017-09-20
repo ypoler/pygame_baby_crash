bdfab6e011f8bc22f22fc7d421d3f09b23129c0c
ypoler <ypoler@yahoo.com>
2017-09-20 14:29:03 +0300

    Changed to shape objects, added fart sound

--- pygame_demo.py
+++ pygame_demo.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame
+import argparse
 from pygame.locals import *
 from random import randint
 
@@ -10,37 +11,15 @@ MIN_SHAPE_SIZE = 100
 COL_WHITE = (255, 255, 255)
 COL_BLACK = (0, 0, 0)
 
-COLOR_ARR = [ (255, 0, 0), # R
-              (0, 255, 0), # G
-			  (0, 0, 255), # B
-			  (255, 255, 0), # Yellow
-              (255, 255, 255), #white
-              (255, 165, 0)  # Orange
- 			  ]
+col_dict = { 'RED' : (255, 0, 0),
+             'GREEN' : (0, 255, 0), 
+			 'BLUE' : (0, 0, 255),
+			 'YELLOW' : (255, 255, 0),
+             'WHITE' : (255, 255, 255),
+             'ORANGE' : (255, 165, 0)  
+ 			}
 
-def draw_circle(surf, x, y, r, col):
-    """
-    Draws a circle
-    """
-    pygame.draw.circle(surf, col, (x, y), r)
-    pygame.draw.circle(surf, COL_WHITE, (x, y), r+1, 1)
-
-
-def draw_rect(surf, x, y, w, h, col):
-    """
-    Draws a rectangle
-    """
-    pygame.draw.rect(surf, col, (x, y, w, h))
-    pygame.draw.rect(surf, COL_WHITE, (x-1, y-1, w+2, h+2), 1)
-
-
-def draw_triangle(surf, x, y, r, col):
-    """
-    Draws a triangle (all sides are same, all 60 degrees
-    """	
-    pygame.draw.polygon(surf, col, ((x, y-r), (x+r*0.86, y+r*0.5), (x-r*0.86, y+r*0.5)) )
-    pygame.draw.polygon(surf, COL_WHITE, ((x, y-1-r), (x+1+r*0.86, y+1+r*0.5), (x-1-r*0.86, y+1+r*0.5)), 1 )
-
+			
 	
 def play_sound(filename):
     """
@@ -48,6 +27,61 @@ def play_sound(filename):
 	"""
     pygame.mixer.music.load(filename)
     pygame.mixer.music.play(0)	
+
+
+	
+class ShapeObject:
+    """
+	Generic shape object
+	"""
+    x = 0
+    y = 0
+    size = 0
+    col = COL_BLACK
+    name = ""
+
+    def __init__(self, x, y, size, col):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.col = col
+
+    def Draw(self, surf):
+        pass	
+
+
+
+class ShapeCircle(ShapeObject):
+    name = "circle"
+
+    def Draw(self, surf):
+		x = self.x
+		y = self.y
+		r = self.size
+		pygame.draw.circle(surf, self.col, (x, y), r)
+		pygame.draw.circle(surf, COL_WHITE, (x, y), r+1, 1)
+	
+
+class ShapeTriangle(ShapeObject):
+    name = "triangle"
+
+    def Draw(self, surf):
+        x = self.x
+        y = self.y
+        r = self.size
+        pygame.draw.polygon(surf, self.col, ((x, y-r), (x+r*0.86, y+r*0.5), (x-r*0.86, y+r*0.5)) )
+        pygame.draw.polygon(surf, COL_WHITE, ((x, y-1-r), (x+1+r*0.86, y+1+r*0.5), (x-1-r*0.86, y+1+r*0.5)), 1 )
+	
+
+class ShapeRectangle(ShapeObject):
+    name = "rectangle"
+
+    def Draw(self, surf):
+        x = self.x
+        y = self.y
+        r = self.size
+        pygame.draw.rect(surf, self.col, (x-r*0.707, y-r*0.707, r*0.707*2, r*0.707*2) )
+        pygame.draw.rect(surf, COL_WHITE, (x-r*0.707-1, y-r*0.707-1, r*0.707*2+2, r*0.707*2+2), 1 )
 	
 
 def draw_random_activity(surf, max_x, max_y, max_size, sound_list):
@@ -60,44 +94,80 @@ def draw_random_activity(surf, max_x, max_y, max_size, sound_list):
     x = randint(MIN_SHAPE_SIZE*2, max_x-MIN_SHAPE_SIZE*2-1)
     y = randint(MIN_SHAPE_SIZE*2, max_y-MIN_SHAPE_SIZE*2-1)
     r = randint(MIN_SHAPE_SIZE, MIN_SHAPE_SIZE+max_size)
-    col = COLOR_ARR[ randint(0, len(COLOR_ARR)-1) ] # (255, 0, 0)
+    col = col_dict.values()[ randint(0, len(col_dict)-1) ]
     activity = randint(0, MAX_ACTIVITY-1)
     
-    sound_file = sound_list[randint(0, len(sound_list)-1)] #"sounds\Jump-SoundBible.com-1007297584.mp3"
+    sound_file = sound_list[randint(0, len(sound_list)-1)]
+	
+    new_shape = ShapeTriangle(x, y, r, col)
 	
     if (activity == 0):
-        draw_circle(surf, x, y, r, col)
+	    new_shape = ShapeCircle(x, y, r, col)
     elif (activity == 1):
-        draw_rect(surf, x, y, r, randint(MIN_SHAPE_SIZE, MIN_SHAPE_SIZE+r-1), col)
+	    new_shape = ShapeRectangle(x, y, r, col)
     elif (activity == 2):
-        draw_triangle(surf, x, y, r, col)
+	    new_shape = ShapeTriangle(x, y, r, col)
 		
+    new_shape.Draw(surf)
     play_sound(sound_file)	
 
+
+			
+def main():
+    """
+	Main game loop - initializes the display, pre-loads resources 
+	and waits for events to activate
+	"""
+	
+    parser = argparse.ArgumentParser(description='Baby smash game')
+    parser.add_argument('--window_mode', dest='window_mode', action='store_true', default='False', 
+	                    help='If set, open the display in window mode instead of full screen')					
+    args = parser.parse_args()
+	
+	
+    """
+    Initialize the display settings based on the display device and args
+    """
+    pygame.init()
+    info = pygame.display.Info()
+    max_x = info.current_w
+    max_y = info.current_h
+    if (args.window_mode == True):
+        max_x = int(max_x*0.75)
+        max_y = int(max_y*0.75)
+        DISPLAYSURF = pygame.display.set_mode( (max_x, max_y) )
+    else:
+        DISPLAYSURF = pygame.display.set_mode( (max_x, max_y), pygame.FULLSCREEN)
+
+    pygame.display.set_caption('Baby Smash')
+
+	
+    """
+    Get the sounds list
+    """
+    sound_list = []
+    for file in os.listdir("sounds"):
+        if file.endswith(".mp3"):
+            sound_list.append(os.path.join("sounds", file))
 		
-pygame.init()
-#DISPLAYSURF = pygame.display.set_mode((400, 300), pygame.FULLSCREEN)
-info = pygame.display.Info()
-
-print(info)
-max_x = info.current_w
-max_y = info.current_h
-DISPLAYSURF = pygame.display.set_mode((max_x, max_y), pygame.FULLSCREEN)
-pygame.display.set_caption('Hello World!')
-
-sound_list = []
-for file in os.listdir("sounds"):
-    if file.endswith(".mp3"):
-        sound_list.append(os.path.join("sounds", file))
 		
-while True: # main game loop
-    for event in pygame.event.get():
-	    if (event.type == KEYDOWN):
-		    draw_random_activity(DISPLAYSURF, max_x, max_y, 200, sound_list)
+		
+	"""
+	Main loop - waits for key and activates an activity 
+	"""
+    while True: # main game loop
+        for event in pygame.event.get():
+	        if (event.type == KEYDOWN):
+		        draw_random_activity(DISPLAYSURF, max_x, max_y, 200, sound_list)
 
-	    elif (event.type == QUIT):
-	        print("Quiting")
-	        pygame.quit()
-	        sys.exit()   
+	        elif (event.type == QUIT):
+	            print("Quiting")
+	            pygame.quit()
+	            sys.exit()   
 
-    pygame.display.update()
+        pygame.display.update()
+	
+			
+		
+if __name__ == '__main__':
+    main()	
